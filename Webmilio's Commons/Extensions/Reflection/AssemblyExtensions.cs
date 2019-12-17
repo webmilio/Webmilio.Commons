605e70364f513e3de47b96ccaf5b96178b48ad99
webmilio <therealwebmilio@gmail.com>
2019-12-16 19:02:25 -0500

    Starting new commons library for all my projects.

---
+++ Webmilio's Commons/Extensions/Reflection/AssemblyExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Webmilio.Commons.Extensions.Reflection
+{
+    public static class AssemblyExtensions
+    {
+        public static IEnumerable<TypeInfo> Concrete(this Assembly assembly) => assembly.DefinedTypes.Concrete();
+
+        public static IEnumerable<TypeInfo> Concrete<T>(this Assembly assembly) => assembly.DefinedTypes.Concrete<T>();
+    }
+}
---
+++ Webmilio's Commons/Extensions/Reflection/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Webmilio.Commons.Extensions.Reflection
+{
+    public static class TypeExtensions
+    {
+        public static IEnumerable<TypeInfo> Concrete(this IEnumerable<TypeInfo> types) => types.Where(t => !t.IsAbstract && !t.IsInterface);
+
+        public static IEnumerable<TypeInfo> Concrete<T>(this IEnumerable<TypeInfo> types) => Concrete(types).Where(t => t.IsSubclassOf(typeof(T)));
+
+
+        public static string NamespaceAsPath(this Type type) => type.Namespace.Replace('.', '\\');
+
+        public static string RootNamespace(this Type type) => type.Namespace.Split('.')[0];
+    }
+}
