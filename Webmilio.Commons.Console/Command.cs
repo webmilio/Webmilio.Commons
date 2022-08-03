47bafd8005ff4741aa7a9d70e59b4e47a792e9e6
webmilio <webmilio@users.noreply.github.com>
2022-08-03 11:29:55 -0400

    Remaking Console library

---
+++ Webmilio.Commons.Console/Command.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Webmilio.Commons.Console;
+
+public interface ICommand
+{
+    public Task Execute(string input, string calledName, string[] args);
+
+    public string Describe();
+
+    public string Name { get; }
+    public string[] Aliases { get; }
+}
+
+public abstract class Command : ICommand
+{
+    public abstract Task Execute(string input, string calledName, string[] args);
+
+    public virtual string Describe()
+    {
+        return string.Empty;
+    }
+
+    public abstract string Name { get; }
+}
---
+++ Webmilio.Commons.Console/SwitchAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Webmilio.Commons.Console;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class SwitchAttribute : Attribute
+{
+    public string[] Aliases { get; set; } = Array.Empty<string>();
+}
